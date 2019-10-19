1957e66e74ba27d4c5b9da758630615e7feedcc5
Grace Galanaga <grace.galanaga@coreproc.ph>
2019-10-20 02:19:25 +0800

    Created event for limiting details

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,47 +1,52 @@
 <template>
-   <div>
-
-	   <div v-for="film in films">
-		   <div>
-			   {{film.title}}
-		   </div>
-		   <div>
-			   {{film.description}}
-		   </div>
-		   <div>
-			   {{film.director}}
-		   </div>
-		   <div>
-			   {{film.producer}}
-		   </div>
-		   <div>
-			   {{film.release_date}}
-		   </div>
-		   <div>
-			   {{film.rt_score}}
-		   </div>
-   		</div>
-   </div>
+  <div>
+	<div>
+		<button v-on:click="limit">Limit to 10</button>
+	</div>
+    <div v-for="film in films" :key="film.id">
+      <div class="max-w-full rounded overflow-hidden shadow-lg">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{{film.title}}</div>
+          <span class="text-gray-700 text-base">Description:</span>
+		  <span class="text-gray-700 text-base">
+			  {{film.description}}
+		  </span>
+		  {{film.director}}
+		  {{film.producer}}
+		  {{film.release_date}}
+		  {{film.rt_score}}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Axios from 'axios'
+import Axios from "axios";
 export default {
-	name: 'MainContent',
-	mounted(){
-		Axios.get(this.BaseUrl+'/films?limit=10').then(
-			(response) => {
-				this.films = response.data
+  name: "MainContent",
+  mounted() {
+    Axios.get(this.BaseUrl + "/films").then(
+	response => {
+      this.films = response.data;
+	});
+	limit();
+  },
+  data() {
+    return {
+      BaseUrl: "https://ghibliapi.herokuapp.com",
+	  films: {},
+	  FilmString: '/films/',
+    };
+  },
+  methods: {
+	  limit(){
+		Axios.get(this.BaseUrl + this.FilmString + "?limit=10").then(
+			reponse => {
+				this.films = reponse.data;
 			}
 		)
-		Axios.get(this.BaseUrl+'')
-	},
-	data(){
-		return {
-			BaseUrl: 'https://ghibliapi.herokuapp.com',
-			
-			films: {}
-		}	
-	}
-}
+	  }
+  }
+};
 </script>
